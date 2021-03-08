791d35faf3919a86014a608b43078a141f90909d
Infiriae <kirkduhon@gmail.com>
2021-03-08 14:02:03 -0600

    JS array project

---
+++ duhon_kirk_arrpredict.js
@@ -0,0 +1,13 @@
+var testArr = [6,3,5,1,2,4]
+var sum = 0;
+
+for(var i = 0; i < testArr.length; i++){
+    sum = sum + testArr[i];
+    console.log("Num: "+testArr[i] +", Sum: "+sum);
+}
+
+for(var i = 0; i < testArr.length; i++){
+    testArr[i] = testArr[i]*i;
+
+}
+console.log(testArr);
