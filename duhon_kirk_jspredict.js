681f0fdb73d3588ae3b13626a203f7607739862d
Infiriae <kirkduhon@gmail.com>
2021-03-08 13:34:14 -0600

    JS prediction

---
+++ duhon_kirk_jspredict.js
@@ -0,0 +1,12 @@
+for(var j = 1; j <= 20; j++){
+    if(j % 2 == 1){
+        console.log(j);
+    }
+}
+
+var sum = 0;
+for(var num = 1; num <= 5; num++){
+  
+  sum = sum + num;
+  console.log("Num: "+num +", Sum: "+sum)
+}
