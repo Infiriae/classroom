958568eb58aadc649495f42b0571a96bf2ac0d8b
Kirk <kirkduhon@gmail.com>
2021-03-27 16:40:12 -0500

    Completed JQueries

--- jqf/jqf.js
+++ jqf/jqf.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
     
     var click = 0;
+    var slide = 0;
+    var theme = 0;
 
     $('td').addClass('ab');
-    
+
     $('th').click(function(){
         var index = $(this).attr('class');
         index = index.split('')[0];
-        if ($('first:td:nth-child('+index+')').attr('class').split(' ')[1] === 'bg-dark'){
-            $('first:td:nth-child('+index+')').removeClass('bg-dark');}
+        if ($('td:nth-child('+index+')').attr('class').split(' ')[1] === 'bg-dark'){
+            $('td:nth-child('+index+')').removeClass('bg-dark');}
         else{
-            $('first:td:nth-child('+index+')').addClass('bg-dark');
+            $('td:nth-child('+index+')').addClass('bg-dark');
         }
         console.log(index);
     });
@@ -19,46 +21,71 @@ $(document).ready(function() {
         alert('You are now leaving this page!');
     });
 
-    $('#you').click(function() {
-        $('#Uname').text('Name?');
+    $('#2head').click(function() {
+        var head = $('#2head').html();
+        $('#2head').html('<h1 id="2head"> Name? '+head+'</h1>');
         console.log('failure');
     });
 
-    $('#show1').click(function() {
-        $('p.thing').show();
+    $('#show').click(function() {
+        $('p.pushitout').toggle();
     });
 
     $('#show2').click(function() {
-        // $('#pushitout').slideToggle();
         if (slide === 0) {
             slide = 1;
-            $('#pushitout').hide('');
-            $('#pushitout2').fadeOut('');
+            $('p.pushitout2').fadeOut('');
             console.log(slide);
         } else {
             slide = 0;
-            $('#pushitout').show('');
-            $('#pushitout2').fadeIn('');
+            $('p.pushitout2').fadeIn('');
             console.log(slide);
         }
+    });
+    
+    $('#show3').hover(function() {
+        alert("Don't hover there");
+    });
+
+    $('#show4').click(function() {
+        // $('#pushitout').slideToggle();
+        if (slide === 0){
+            slide = 1;
+            $('div.slides').slideUp();
+        } else {
+            slide = 0;
+            $('div.slides').slideDown();
+        }
+    });
+
+    $('#show5').click(function() {
+        $('p.spam').after('<p>spam</p>');
+    });
+    var gen = 0;
+    $('input').click(function(){
+        gen++;
+        $('p.spam').before('<p>'+gen+'</p>');
+        $('p.spam').append($(this).val()+' ');
+        console.log($(this).val());
     });
 
     $('#theme').click(function() {
         // $('#pushitout').slideToggle();
         if (theme === 0) {
             theme = 1;
-            $('#theme').text('No change it back.');
-            $('p, h1').addClass('text-light');
-            $('body').removeClass('bg-info');
+            $('#theme').text('Change it back.');
+            $('p, h1, th, td, label').addClass('text-light');
+            $('p.theme').text('This is Dark.');
             $('body').addClass('bg-dark');
             console.log(theme);
         } else {
             theme = 0;
-            $('#theme').text('Nevermind. I like it dark.');
-            $('p, h1').removeClass('text-light');
+            $('#theme').text('I like it dark.');
+            $('p, h1, th, td, label').removeClass('text-light');
+            $('p.theme').text('This is Light.');
             $('body').removeClass('bg-dark');
-            $('body').addClass('bg-info');
             console.log(theme);
+            console.log($('p.theme').attr('html'));
         }
     });
 
