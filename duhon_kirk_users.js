3794ed182de71c12ff785436a6267fedbbafa72f
Infiriae <kirkduhon@gmail.com>
2021-03-28 18:32:47 -0500

    Completed users function. DOesn't look dry enough

--- duhon_kirk_users.js
+++ duhon_kirk_users.js
@@ -32,19 +32,41 @@ users = [
         }
     }
 ]
+var loop = 0;
 
-function userLanguages(arr) {
-    for(var i = 0; i < arr.length; i++){    
-        for(var key in arr[i]){
-            if(typeof(arr[i][key]) === 'object'){
-                userLanguages(arr[i][key]);
+
+function userLanguages(data){
+    for(var i in data){
+        var mod = [];
+        var s = '';
+        var z = '';
+        s = (data[i].fname + " " + data[i].lname + " knows ");
+        var lle = data[i].languages.length;
+        for(var o in data[i].languages){
+            if(o === String((lle - 1))){
+                s = (s + "and " + data[i].languages[o] + ".");
+            } else {
+                s = (s + data[i].languages[o] + ", ")
             }
-            //console.log(typeof(arr[i][key]));
-            console.log(arr[i][key]);
         }
-    }
-}
+        z = (z + data[i].fname + ' is also interested in ')
+        for (var o in data[i].interests){
+            for(var p in data[i].interests[o]){
+                mod.push(data[i].interests[o][p]);
+            }
+        }
+            for(var y = 0; y < mod.length; y++){
+                if(y === (mod.length-1)){
+                    z = (z + "and " + mod[y] + "." );
+                } else {
+                    z = (z + mod[y] + ", ");
+                }
+            }
+            console.log(s);
+            console.log(z);
+        }
 
+}
 userLanguages(users);
 
 // function userLanguages(arr) {
@@ -70,3 +92,27 @@ userLanguages(users);
 //         }
 //     }
 // }
+
+// function userLanguages(arr,key) {
+    //     if(typeof(arr) === 'object') {
+    //         if(Array.isArray(arr) === 'true'){
+    //             console.log('Array');
+    //             for(var i = 0; i < arr.length; i++) {
+    //                 for(var o in arr[i]) {
+    //                     if(typeof(arr[i][o]) === 'string') {
+    //                         console.log(arr[i][o]);
+    //                     }
+    //                 }
+    //             }
+    //         }  else {
+    //             for(var p in arr){
+    //                 if(typeof(p) === 'string' && p.length > 2) {
+    //                     var key = p;
+    //                 }
+    //                 userLanguages(arr[p],key); 
+    //             }
+    //         }
+    //     } else {
+    //         console.log(key + " " + arr);
+    //     }  
+    // }
